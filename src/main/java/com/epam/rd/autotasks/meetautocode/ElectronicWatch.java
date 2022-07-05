261ae34bdbceb670ffd4858947d5d9c18af3ae78
Daniil Poleshchenko <danilapoleshchenko@gmail.com>
2022-07-05 20:01:32 +0300

    Electronic Watch program

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,7 +6,13 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
-
-
+        int hoursClock = seconds / 3600;
+        int minutesClock = seconds / 60 - hoursClock * 60;
+        int secondsClock = seconds - hoursClock * 3600 - minutesClock * 60;
+        int hoursClockNext = (hoursClock >= 24) ? hoursClock - 24 * (seconds / 86400) : hoursClock;
+        String secondsClockText = (secondsClock >= 10 ? "" : "0") + secondsClock;
+        String minutesClockText = (minutesClock >= 10 ? "" : "0") + minutesClock;
+        System.out.println(hoursClockNext + ":" + minutesClockText + ":" + secondsClockText);
     }
 }
+
